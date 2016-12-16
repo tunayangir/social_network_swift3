579dd138db68b96578da4dda145c291551cb233a
tunayangir <tunayangir@hotmail.com>
2016-12-16 23:48:44 +0200

    1

---
+++ SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social_network
+//
+//  Created by TunaYANGIR on 15.12.2016.
+//  Copyright © 2016 TunaYANGIR. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
